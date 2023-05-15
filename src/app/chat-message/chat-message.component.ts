3f472387aaa6b0e8d07445ea3d75ce2333a18620
Lancer C ZHANG <lancerczhang@hsbc.com>
2023-05-15 15:47:12 +0800

    regenerate response

--- src/app/chat-message/chat-message.component.ts
+++ src/app/chat-message/chat-message.component.ts
@@ -6,8 +6,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./chat-message.component.scss']
 })
 export class ChatMessageComponent {
-  @Input() message: { sender: string, text: string, isPrompt: boolean, isLoading?: boolean };
-  @Output() addPrompt: EventEmitter<any> = new EventEmitter();
+  @Input() message: { sender: string, text: string, isPrompt: boolean, isLoading?: boolean, isLast?: boolean };
+  @Output() addPrompt = new EventEmitter<any>();
+  @Output() regenerateResponse = new EventEmitter<any>();
+
 
   constructor() { }
 
--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import { ChatApiService } from '../services/chat-api.service';
 import { ChatDbService } from '../services/chat-db.service';
 import { PromptService } from '../services/prompt.service';
@@ -12,12 +14,13 @@ import { environment } from './../../environments/environment';
   styleUrls: ['./chat-window.component.scss']
 })
 export class ChatWindowComponent {
-  messages: { id?: number, sender: string, text: string, numTokens: number, isPrompt: boolean, isLoading?: boolean }[] = [];
+  messages: { id?: number, sender: string, text: string, numTokens: number, isPrompt: boolean, isLoading?: boolean, isLast?: boolean }[] = [];
   inputMessage: string = '';
   conversationId: number;
   promptOptions: { id: string; act: string; prompt: string }[] = [];
 
   constructor(
+    private dialog: MatDialog,
     private promptService: PromptService,
     private sharedService: SharedService,
     private route: ActivatedRoute,
@@ -39,6 +42,30 @@ export class ChatWindowComponent {
 
   async loadChat() {
     this.messages = await this.chatDb.getMessages(this.conversationId);
+
+    let lastUserMessageIndex = -1;
+    let lastBotMessageIndex = -1;
+
+    this.messages.forEach((message, index) => {
+      if (message.sender === 'user') {
+        if (lastUserMessageIndex !== -1) {
+          this.messages[lastUserMessageIndex].isLast = false;
+        }
+        lastUserMessageIndex = index;
+      } else if (message.sender === 'bot') {
+        if (lastBotMessageIndex !== -1) {
+          this.messages[lastBotMessageIndex].isLast = false;
+        }
+        lastBotMessageIndex = index;
+      }
+    });
+
+    if (lastUserMessageIndex !== -1) {
+      this.messages[lastUserMessageIndex].isLast = true;
+    }
+    if (lastBotMessageIndex !== -1) {
+      this.messages[lastBotMessageIndex].isLast = true;
+    }
   }
 
   async sendMessage() {
@@ -49,25 +76,33 @@ export class ChatWindowComponent {
         this.sharedService.emitRefreshChatHistory();
       }
 
+      // Reset all isLast flags
+      for (const message of this.messages) {
+        message.isLast = false;
+      }
+
       // Prepare the input for getResponse
       const messageArray = this.prepareMessages(this.inputMessage);
+
+      // update messages of pages
       const userMessageId: number = await this.chatDb.createMessage({ conversationId: this.conversationId, sender: 'user', text: this.inputMessage });
-      const userMessage = { id: userMessageId, sender: 'user', text: this.inputMessage, numTokens: 0, isPrompt: false };
+      const userMessage = { id: userMessageId, sender: 'user', text: this.inputMessage, numTokens: 0, isPrompt: false, isLast: true };
+      const botMessage = { id: -1, sender: 'bot', text: '', numTokens: 0, isPrompt: false, isLast: true, isLoading: true };
+
+      this.messages.push(userMessage);
+      this.messages.push(botMessage);
 
       // Send the array of messages to getResponse
       this.chatApiService.getResponse(messageArray).subscribe(async (response: any) => {
         const respContent = response.data.choices[0].message.content;
 
-        const botMessageId: number = await this.chatDb.createMessage({ conversationId: this.conversationId, sender: 'bot', text: respContent });
-        const botMessage = { id: botMessageId, sender: 'bot', text: '', numTokens: 0, isPrompt: false, isLoading: true };
-
         userMessage.numTokens = response.data.usage.prompt_tokens;
+
+        const botMessageId: number = await this.chatDb.createMessage({ conversationId: this.conversationId, sender: 'bot', text: respContent });
+        botMessage.id = botMessageId
         botMessage.numTokens = response.data.usage.completion_tokens;
         botMessage.text = respContent;
         botMessage.isLoading = false;
-
-        this.messages.push(userMessage);
-        this.messages.push(botMessage);
       });
 
       this.inputMessage = '';
@@ -102,7 +137,6 @@ export class ChatWindowComponent {
   }
 
   onInputChange(): void {
-    console.log("this.inputMessage", this.inputMessage)
     if (this.inputMessage.startsWith('/')) {
       const query = this.inputMessage.slice(1);
       this.promptOptions = this.promptService.searchPrompts(query);
@@ -114,9 +148,23 @@ export class ChatWindowComponent {
   }
 
   addPrompt(message: any) {
-    message.isPrompt = true;
-    this.chatDb.updateIsPrompt(message.id, true);
-    this.promptService.addPrompt({ act: '', prompt: message.text });
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: { actionName: "Add", reourceName: "prompt" }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        message.isPrompt = true;
+        this.chatDb.updateIsPrompt(message.id, true);
+        this.promptService.addPrompt({ act: '', prompt: message.text });
+      }
+    });
   }
 
+  regenerateResponse() {
+    this.inputMessage = this.messages[this.messages.length - 2].text;
+    this.messages.splice(-2);
+    this.sendMessage();
+  }
 }
