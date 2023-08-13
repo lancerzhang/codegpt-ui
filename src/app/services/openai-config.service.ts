d8a7fcaec896bdf22568f768607055f5345c0a01
lancerzhang <lancerz@hotmail.com>
2023-08-13 15:39:31 +0800

    dummy config

--- src/app/services/openai-config.service.ts
+++ src/app/services/openai-config.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
@@ -22,15 +22,33 @@ export class OpenaiConfigService {
   }
 
   private fetchOpenAIConfig(): Observable<any> {
-    return this.http
-      .get(`${environment.apiOthersBase}/config`)
-      .pipe(
-        map((data: any) => {
-          data.selectedModel = data.models[0]; // Select the first model by default
-          return data;
-        }),
-        tap((data) => this.openaiSubject.next(data))
-      );
+    var response;
+    if (environment.useDummy) {
+      response = of({
+        "apiVersion": "2023-05-15",
+        "models": [{
+          "model": "GTP-3.5",
+          "contextLength": 4096,
+          "deployment": "WpbcognitiveGptTurboModel",
+          "maxTokens": null, "session": {
+            "chats": 10,
+            "tokens": 1300
+          },
+          "rateLimit": null
+        }]
+      });
+    } else {
+      response = this.http
+        .get(`${environment.apiOthersBase}/config`);
+    }
+
+    return response.pipe(
+      map((data: any) => {
+        data.selectedModel = data.models[0]; // Select the first model by default
+        return data;
+      }),
+      tap((data) => this.openaiSubject.next(data))
+    );
   }
 
   getOpenAIConfig(): Observable<any> {
